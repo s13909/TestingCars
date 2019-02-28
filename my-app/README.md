4438cf5ca768a38878d67ae16b56629bb558db2e
jaruzzzelski <s13909@pjwstk.edu.pl>
2019-02-28 19:02:54 +0100

    Refactor

---
+++ my-app/README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/s13909/TestingCars.svg?branch=master)](https://travis-ci.org/s13909/TestingCars)
+# Testing Cars
+Subject: Cars
---
+++ my-app/src/main/java/com/lab01/app/Car.java
@@ -0,0 +1,12 @@
+package com.lab01.app;
+public class Car
+{
+    public static void main( String[] args )
+    {
+        String make;
+        String model;
+        String color;
+        int vin;
+
+    }
+}
